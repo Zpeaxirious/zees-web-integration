514fa38434f3edc42f94ffb48bf3954302664b1e
Zpeaxirious <zpeaxirious@protonmail.com>
2025-06-24 20:47:12 +0200

    first commit

---
+++ src/client/java/net/yumeverse/zeeswebintegration/ZeesWebIntegrationClient.java
@@ -0,0 +1,10 @@
+package net.yumeverse.zeeswebintegration;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ZeesWebIntegrationClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
